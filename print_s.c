0e3ba821176b78ec83dbdded5d36c23b20ca693f
Hydroponey <hydroponey@localhost>
2020-06-22 01:10:13 +0200

    print_s add null check

--- print_s.c
+++ print_s.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 11:32:06 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/22 01:08:59 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/22 01:10:06 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	print_s(va_list args, t_specifier *specifier, int *count)
 	char	*str;
 	
 	str = (char*)va_arg(args, char *);
+	if (!str)
+		return ;
 	*count += ft_strlen(str);
 	ft_putstr_fd(str, 1);
 }
