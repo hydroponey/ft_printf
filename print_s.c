778c354723532ab0925ae723ff434e6426a7ba02
Hydroponey <hydroponey@localhost>
2020-06-22 01:09:13 +0200

    print_s lazy try

--- print_s.c
+++ print_s.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 11:32:06 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/21 12:06:45 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/22 01:08:59 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 
 void	print_s(va_list args, t_specifier *specifier, int *count)
 {
+	char	*str;
 	
+	str = (char*)va_arg(args, char *);
+	*count += ft_strlen(str);
+	ft_putstr_fd(str, 1);
 }
