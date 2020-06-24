cbd3c6dd4900d1b1cce9ee71feb279e2af29856a
Hydroponey <hydroponey@localhost>
2020-06-24 08:29:55 +0200

    print_s handling width and precision and null ptr

--- print_s.c
+++ print_s.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 11:32:06 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/22 01:10:06 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/24 08:29:00 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,25 @@
 void	print_s(va_list args, t_specifier *specifier, int *count)
 {
 	char	*str;
+	int		len;
 	
 	str = (char*)va_arg(args, char *);
-	if (!str)
+	if (str == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		*count += 6;
 		return ;
+	}
+	len = ft_strlen(str);
+	if (specifier->precision != -1 && specifier->precision < len)
+	{
+		str = ft_substr(str, 0, specifier->precision);
+		len = specifier->precision;
+	}
+	if (specifier->flags & FLAG_MINUS && specifier->width > len)
+		str = pad_left(' ', specifier->width - len, str);
+	else if (specifier->width > len)
+		str = pad_right(' ', specifier->width - len, str);
 	*count += ft_strlen(str);
 	ft_putstr_fd(str, 1);
 }
