ad90c9d89f916875e94649b0f51178acdacf6308
Hydroponey <hydroponey@localhost>
2020-06-24 11:18:42 +0200

    swap padding error in print_s

--- print_s.c
+++ print_s.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 11:32:06 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/24 10:37:37 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/24 11:18:22 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ void	print_s(va_list args, t_specifier *specifier, int *count)
 		len = specifier->precision;
 	}
 	if (specifier->flags & FLAG_MINUS && specifier->width > len)
-		str = pad_left(' ', specifier->width - len, str);
-	else if (specifier->width > len)
 		str = pad_right(' ', specifier->width - len, str);
+	else if (specifier->width > len)
+		str = pad_left(' ', specifier->width - len, str);
 	*count += ft_strlen(str);
 	ft_putstr_fd(str, 1);
 }
