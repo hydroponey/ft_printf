d46b6a2c4b26eb14fec8d930fbbdebfa3d94011f
Hydroponey <hydroponey@localhost>
2020-06-24 11:54:19 +0200

    modify to free if the pointer is allocated by our functions

--- print_s.c
+++ print_s.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 11:32:06 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/24 11:25:03 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/24 11:53:57 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 
 void	print_s(va_list args, t_specifier *specifier, int *count)
 {
-	char	*str;
-	char	*tmp;
-	int		len;
+	char		*str;
+	char		*tmp;
+	int			len;
+	short int	free_ptr;
 	
+	free_ptr = 0;
 	str = (char*)va_arg(args, char *);
 	if (str == NULL)
 	{
@@ -27,6 +29,7 @@ void	print_s(va_list args, t_specifier *specifier, int *count)
 			*count = -1;
 			return ;
 		}
+		free_ptr = 1;
 		ft_strlcpy(str, "(null)", 7);
 	}
 	len = ft_strlen(str);
@@ -36,9 +39,17 @@ void	print_s(va_list args, t_specifier *specifier, int *count)
 		len = specifier->precision;
 	}
 	if (specifier->flags & FLAG_MINUS && specifier->width > len)
+	{
+		tmp = (free_ptr) ? str : NULL;
 		str = pad_right(' ', specifier->width - len, str);
+		free(tmp);
+	}
 	else if (specifier->width > len)
+	{
+		tmp = (free_ptr) ? str : NULL;
 		str = pad_left(' ', specifier->width - len, str);
+		free(tmp);
+	}
 	*count += ft_strlen(str);
 	ft_putstr_fd(str, 1);
 }
