3f23eb696ae1787b5535579ad974a75c787539d2
Hydroponey <hydroponey@localhost>
2020-06-25 06:16:30 +0200

    print_u start test

--- print_u.c
+++ print_u.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/21 12:06:40 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/25 06:16:14 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 
 void	print_u(va_list args, t_specifier *specifier, int *count)
 {
-	
+	unsigned int	number;
+	char			*number_str;
+
+	number = (unsigned int)va_arg(args, unsigned int);
+	number_str = ft_itoa(number);
+	ft_putstr_fd(number_str, 1);
 }
