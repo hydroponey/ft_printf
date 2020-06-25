63f5b9eec8017449ee95523c99528175c00444fa
Hydroponey <hydroponey@localhost>
2020-06-25 03:14:18 +0200

    print_p handle null ptr and return count

--- print_p.c
+++ print_p.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/21 18:34:14 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/06/25 03:14:02 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,26 @@ char hex_digit(int v) {
         return 'a' + v - 10; // <-- Here
 }
 
-void print_address_hex(void* p0) {
-    int i;
-    unsigned long p = (unsigned long)p0;
-	int leading_zero = 1;
+int		print_address_hex(void* p0) {
+    int 			i;
+    unsigned long 	p;
+	int 			leading_zero;
+	short int		count;
 
-    write_char('0'); write_char('x');
+	p = (unsigned long)p0;
+	leading_zero = 1;
+	count = 2;
+    write_char('0');
+	write_char('x');
     for(i = (sizeof(p) << 3) - 4; i>=0; i -= 4) {
 		if (leading_zero && (hex_digit((p >> i) & 0xf)) == '0')
 			continue;
 		else
 			leading_zero = 0;
         write_char(hex_digit((p >> i) & 0xf));
+		count++;
     }
+	return (count);
 }
 
 void	print_p(va_list args, t_specifier *specifier, int *count)
@@ -45,6 +52,13 @@ void	print_p(va_list args, t_specifier *specifier, int *count)
 	void	*ptr;
 
 	ptr = (void*)va_arg(args, void*);
-
-	print_address_hex(ptr);
+	if (ptr == NULL)
+	{
+		ft_putstr_fd("(null)", 6);
+		*count += 6;
+	}
+	else
+	{
+		*count += print_address_hex(ptr);
+	}
 }
