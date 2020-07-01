3d3481b145de1c9adeb186b566f92f68d56dc641
Hydroponey <hydroponey@localhost>
2020-07-01 16:28:29 +0200

    copy print_d content in print_u and change sign

--- print_u.c
+++ print_u.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/06/25 11:39:31 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/07/01 16:27:46 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,12 +53,93 @@ char			*ft_utoa(unsigned int n)
 	return (nbr);
 }
 
-void	print_u(va_list args, t_specifier *specifier, int *count)
+char		*d_pad_left(char c, int n, char *data)
 {
-	unsigned int	number;
-	char			*number_str;
+	char	padding[n + 1];
+	char	*padded_string;
+
+	ft_bzero(padding, n + 1);
+	while (n--)
+		padding[n] = c;
+	padded_string = ft_strjoin(padding, data);
+	free(data);
+	return (padded_string);
+}
+
+char		*d_pad_right(char c, int n, char *data)
+{
+	char	padding[n + 1];
+	char	*padded_string;
+
+	ft_bzero(padding, n + 1);
+	while (n--)
+		padding[n] = c;
+	padded_string = ft_strjoin(data, padding);
+	free(data);
+	return (padded_string);
+}
+
+char		*set_precision(char *num_str, unsigned int num, int precision, int *len)
+{
+	char	*newstr;
+
+	newstr = num_str;
+	if (precision != -1 && precision > *len)
+	{
+		newstr = pad_left('0', precision - *len, num_str);
+		*len = ft_strlen(newstr);
+	}
+	return (newstr);
+}
+
+char		*set_width(char *number_str, unsigned int number, t_specifier *specifier, int *len)
+{
+	if (specifier->is_width && specifier->width < 0)
+	{
+		specifier->width *= -1;
+		if ((specifier->flags & FLAG_MINUS) == 0)
+			specifier->flags ^= FLAG_MINUS;
+	}
+	if (*len < specifier->width)
+	{
+		if (specifier->flags & FLAG_MINUS)
+			number_str = d_pad_right(' ', specifier->width - *len, number_str);
+		else
+		{
+			if (specifier->flags & FLAG_ZERO && specifier->precision == -1)
+				number_str = d_pad_left('0', specifier->width - *len, number_str);
+			else
+				number_str = d_pad_left(' ', specifier->width - *len, number_str);
+		}
+	}
+	return (number_str);
+}
+
+void		print_u(va_list args, t_specifier *specifier, int *count)
+{
+	unsigned int		number;
+	char				*number_str;
+	int					len;
 
 	number = (unsigned int)va_arg(args, unsigned int);
-	number_str = ft_utoa(number);
+	if (!(number_str = ft_utoa(number)))
+	{
+		*count = -1;
+		return ;
+	}
+	len = ft_strlen(number_str);
+	number_str = set_precision(number_str, number, specifier->precision, &len);
+	if (specifier->flags & FLAG_PLUS && number > 0)
+	{
+		number_str = d_pad_left('+', 1, number_str);
+		len++;
+	}
+	if (specifier->precision == 0 && number == 0)
+		number_str[--len] = '\0';
+	number_str = set_width(number_str, number, specifier, &len);
+	if (number > 0 && len > specifier->width && specifier->flags & FLAG_SPACE)
+		number_str = d_pad_left(' ', 1, number_str);
 	ft_putstr_fd(number_str, 1);
+	*count += ft_strlen(number_str);
+	free(number_str);
 }
