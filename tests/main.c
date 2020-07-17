e4f9a20e2812c8d74cd2a7b3b424c22d772cf8b9
Hydroponey <hydroponey@localhost>
2020-07-17 16:21:25 +0200

    modify tests to see the results better

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/03 11:46:13 by asimoes           #+#    #+#             */
-/*   Updated: 2020/07/17 15:45:59 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/07/17 16:21:19 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -195,12 +195,13 @@ int main()
 	
 	
 	// %p tests
+	// WRONG------------------------------------------------------
 	printf("printf:\t\t|");
 	real = printf("%-16.p", &ptr);
-	printf("|\t%d\n", real);
+	printf("|\t%d\tWRONG\n", real);
 	ft_printf("ft_printf:\t|");
 	mine = ft_printf("%-16.p", &ptr);
-	ft_printf("|\t%d\n", mine);
+	ft_printf("|\t%d\tWRONG\n", mine);
 	printf("---------------------------------------------------\n");
 	// ----------------------------------------------------------
 	printf("printf:\t\t|");
@@ -218,13 +219,13 @@ int main()
 	mine = ft_printf("%-16p", &ptr);
 	ft_printf("|\t%d\n", mine);
 	printf("---------------------------------------------------\n");
-	// ----------------------------------------------------------
+	// WRONG------------------------------------------------------
 	printf("printf:\t\t|");
 	real = printf("%-15.p", &ptr);
-	printf("|\t%d\n", real);
+	printf("|\t%d\tWRONG\n", real);
 	ft_printf("ft_printf:\t|");
 	mine = ft_printf("%-15.p", &ptr);
-	ft_printf("|\t%d\n", mine);
+	ft_printf("|\t%d\tWRONG\n", mine);
 	printf("---------------------------------------------------\n");
 
 
