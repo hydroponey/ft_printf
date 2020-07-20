84efa9dbbce39d2d79e6dc9a2da7fb584f74a80d
Hydroponey <hydroponey@localhost>
2020-07-20 23:14:12 +0200

    print_d test

--- print_d.c
+++ print_d.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/07/20 23:12:11 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/07/20 23:14:06 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,7 @@ char		*set_width(char *str, int number, t_specifier *specifier, int num_len, int
 		str = pad_right(' ', specifier->width - *len, str, 1);
 	else
 	{
-		if (specifier->flags & FLAG_ZERO &&
-			specifier->is_precision == 1 && specifier->precision > 0)
+		if (specifier->flags & FLAG_ZERO &&	specifier->is_precision == 0)
 		{
 			str = pad_left('0', specifier->width - *len, str, 1);
 			if (number < 0)
