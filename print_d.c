6a57160b7d821eb1fdf07c0f3e08fa659bf8767e
Hydroponey <hydroponey@localhost>
2020-07-20 17:07:36 +0200

    print_d use is_precision to check for precision

--- print_d.c
+++ print_d.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/07/20 17:07:00 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/07/20 17:07:23 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char		*set_width(char *str, int number, t_specifier *specifier, int *len)
 		str = pad_right(' ', specifier->width - *len, str, 1);
 	else
 	{
-		if (specifier->flags & FLAG_ZERO && specifier->precision == -1)
+		if (specifier->flags & FLAG_ZERO && specifier->is_precision == 0)
 		{
 			str = pad_left('0', specifier->width - *len, str, 1);
 			if (number < 0)
