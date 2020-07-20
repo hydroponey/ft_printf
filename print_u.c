c90a6051b124c652d45156c68e5e1a147f5e29a1
Hydroponey <hydroponey@localhost>
2020-07-20 23:32:08 +0200

    print_u fix precision and width

--- print_u.c
+++ print_u.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/07/20 23:28:30 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/07/20 23:31:53 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,9 @@ static char			*set_width(char *str, t_specifier *specifier, int *len)
 			str = pad_right(' ', specifier->width - *len, str, 1);
 		else
 		{
-			if (specifier->flags & FLAG_ZERO && specifier->precision == -1)
+			if (specifier->flags & FLAG_ZERO &&
+			(specifier->is_precision == 0 || (specifier->is_precision == 1
+			&& specifier->precision < 0)))
 				str = pad_left('0', specifier->width - *len, str, 1);
 			else
 				str = pad_left(' ', specifier->width - *len, str, 1);
