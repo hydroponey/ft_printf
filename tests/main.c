c023a41a6f85f758a4732984aa345f883a5a47a2
Hydroponey <hydroponey@localhost>
2020-07-21 23:27:58 +0200

    new %s tests

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/03 11:46:13 by asimoes           #+#    #+#             */
-/*   Updated: 2020/07/21 23:27:05 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/07/21 23:27:57 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -365,6 +365,14 @@ int main()
 	printf("|\t%d\n", real);
 	ft_printf("ft_printf:\t|");
 	mine = ft_printf("%---2s", NULL);
+	ft_printf("|\t%d\n", mine);
+	printf("---------------------------------------------------\n");
+	printf("specifier:\t|%%.*s| %d %s\n", -2, NULL);
+	printf("printf:\t\t|");
+	real = printf("%.*s", -2, NULL);
+	printf("|\t%d\n", real);
+	ft_printf("ft_printf:\t|");
+	mine = ft_printf("%.*s", -2, NULL);
 	ft_printf("|\t%d\n", mine);
 	return (0);
 
