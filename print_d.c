e578ab3417efd81e902c93fa407d77164ccfcff0
Hydroponey <hydroponey@localhost>
2020-08-05 22:50:47 +0200

    print_d test for fixing %5+d

--- print_d.c
+++ print_d.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/05 22:50:00 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/08/05 22:50:43 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ static char		*set_width(char *str, int num, t_s_data *specifier, int *len)
 	if (specifier->flags & FLAG_PLUS && num >= 0)
 	{
 		str = pad_left('+', 1, str);
-		*len++;
+		(*len)++;
 	}
 	if (specifier->flags & FLAG_MINUS)
 		str = pad_right(' ', specifier->width - *len, str);
