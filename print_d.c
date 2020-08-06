e550d04fcd2d6e178503285bc0c397ac78d8cd13
Hydroponey <hydroponey@localhost>
2020-08-06 19:12:57 +0200

    print_d fix flag plus when flag zero

--- print_d.c
+++ print_d.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/06 19:11:51 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/08/06 19:12:46 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ static char		*set_width(char *str, int num, t_s_data *specifier, int *len)
 		}
 		return (str);
 	}
-	if (specifier->flags & FLAG_PLUS && num >= 0)
+	if (specifier->flags & FLAG_PLUS && num >= 0 && (specifier->flags & FLAG_ZERO) == 0)
 	{
 		specifier->width--;
 		str = pad_left('+', 1, str);
