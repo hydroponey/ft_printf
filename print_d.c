4f5a2cbad0fdd2b228105762b5f0653ab1bfb537
Hydroponey <hydroponey@localhost>
2020-08-07 00:03:16 +0200

    print_d improve set_precision

--- print_d.c
+++ print_d.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 07:41:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/07 00:01:54 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/08/07 00:03:15 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ static char		*set_width(char *str, int num, t_s_data *specifier, int *len)
 		return (str);
 	}
 	if (specifier->flags & FLAG_PLUS && num >= 0
-	&& ((specifier->flags & FLAG_ZERO) == 0 || (specifier->flags & FLAG_ZERO) && (specifier->flags & FLAG_MINUS)))
+	&& ((specifier->flags & FLAG_ZERO) == 0 || ((specifier->flags & FLAG_ZERO) && (specifier->flags & FLAG_MINUS))))
 	{
 		specifier->width--;
 		str = pad_left('+', 1, str);
