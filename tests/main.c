710fd2ccdb3cbb8a7874a53e5f5256271360aa9d
Hydroponey <hydroponey@localhost>
2020-08-06 19:34:22 +0200

    test file add one test

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/03 11:46:13 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/05 22:48:31 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/08/06 19:32:20 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -557,6 +557,22 @@ int main()
 	printf("|\t%d\n", real);
 	ft_printf("ft_printf:\t|");
 	mine = ft_printf("%-5+d", -42);
+	ft_printf("|\t%d\n", mine);
+	printf("---------------------------------------------------\n");
+	printf("specifier:\t|%%-0+5d| 42\n");
+	printf("printf:\t\t|");
+	real = printf("%-0+5d", 42);
+	printf("|\t%d\n", real);
+	ft_printf("ft_printf:\t|");
+	mine = ft_printf("%-0+5d", 42);
+	ft_printf("|\t%d\n", mine);
+	printf("---------------------------------------------------\n");
+	printf("specifier:\t|%% 03d| 0\n");
+	printf("printf:\t\t|");
+	real = printf("% 03d", 0);
+	printf("|\t%d\n", real);
+	ft_printf("ft_printf:\t|");
+	mine = ft_printf("% 03d", 0);
 	ft_printf("|\t%d\n", mine);
 	return (0);
 
